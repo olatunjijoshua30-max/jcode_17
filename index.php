05ea85754d0ca92f054674e9536df6ce63d94f77
JCODE <olatunjijoshua30@gmail.com>
2026-02-01 10:38:06 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>assignment</title>
+</head>
+<body>
+    
+    <?php
+        // $n = readline ("enter a number:");
+        // $x = 2;
+        // $sum = 0;
+       
+        // while ($x <= $n ){
+        // echo "$x <br>";
+        // $sum += $x;
+        // $x = $x + 2;
+        // }
+        //  echo "the sum even numbers : $sum <br>";
+    ?>
+
+    <?php
+    for ($x = 1; $x <= 40; $x++;){
+        echo '$x';
+    }
+    ?>
+
+</body>
+</html>
